3f867db3d39aaec4d32001bf1ac7efe04aa418d6
MD-YUSUF-22 <mdyusuf47461@gmail.com>
2024-02-20 19:44:43 +0600

    Seat booking sub-section updated

--- assignment.js
+++ assignment.js
@@ -4,3 +4,17 @@ buyTicketBtn.addEventListener('click', function(){
     const ticket=document.getElementById('ticket');
     ticket.scrollIntoView({behavior: "smooth"})
 })
+ let count=0;
+const seatChoice= document.getElementsByClassName('seat-choice');
+for(let seats of seatChoice ){
+    count=count+1;
+    // if(count>4){
+    //     alert('Maximum selection limit reached')
+    // }
+    seats.addEventListener('click', function(){
+        seats.style.backgroundColor='#1DD100';
+    });
+    
+    console.log(count);
+}
+
