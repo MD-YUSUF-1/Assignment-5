754284791c96300390ec47030da206ccba2a8820
MD-YUSUF-22 <mdyusuf47461@gmail.com>
2024-02-20 17:19:28 +0600

    Sit choice sub-section completed and Scrool js added

--- assignment.js
+++ assignment.js
@@ -1,1 +1,6 @@
-console.log("Yusuf")
+const buyTicketBtn= document.getElementById('buy-ticket-btn');
+
+buyTicketBtn.addEventListener('click', function(){
+    const ticket=document.getElementById('ticket');
+    ticket.scrollIntoView({behavior: "smooth"})
+})
