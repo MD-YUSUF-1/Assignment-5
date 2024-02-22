f5a5cce1970632f8561392921e124d8d6305360d
MD-YUSUF-22 <mdyusuf47461@gmail.com>
2024-02-22 14:25:25 +0600

    Final commit 1.0

--- js/assignment.js
+++ js/assignment.js
@@ -1,29 +1,37 @@
-const buyTicketBtn = document.getElementById('buy-ticket-btn');
 
+// Buy ticket button Code
+const buyTicketBtn = document.getElementById('buy-ticket-btn');
 buyTicketBtn.addEventListener('click', function () {
     const ticket = document.getElementById('ticket');
     ticket.scrollIntoView({ behavior: "smooth" })
 })
 
+// Seat choice code
 let count = 0;
 const seatChoice = document.getElementsByClassName('seat-choice');
 for (let seats of seatChoice) {
     seats.addEventListener('click', function () {
+        // Multiple click problem solution code
+        if(seats.style.backgroundColor==='rgb(29, 209, 0)'){
+            return;
+        }
+        
+        // seat color change code
         count = count + 1;
         seats.style.backgroundColor = '#1DD100';
-
         if(count>4){
             alert('Maximum selected seats reach')
             seats.style.backgroundColor = '';
             return;
         }
-        document.getElementById('selected-seat').innerText = count;
 
+        // seat number counter code
+        document.getElementById('selected-seat').innerText = count;
         document.getElementById('left-seats').innerText = 8 - count;
 
+        // Selected seats Adding in Seat Booking section
         const seatText = seats.innerText;
         const seatPrice = document.getElementById('seat-price').innerText;
-        console.log(seatPrice);
         const selectedContainer = document.getElementById('choice-seats-details')
         const li = document.createElement("li");
         const seatNumber = document.createElement("p");
@@ -32,23 +40,26 @@ for (let seats of seatChoice) {
         price.innerText = seatPrice;
         const seatType = document.createElement("p");
         seatType.innerText = "Economy"
+
         li.appendChild(seatNumber);
         li.appendChild(seatType);
         li.appendChild(price);
         selectedContainer.appendChild(li);
 
+        // total price calculator
         const totalPrice = document.getElementById('total-price').innerText;
         const convertedTotalPrice = parseInt(totalPrice)
         document.getElementById('total-price').innerText = convertedTotalPrice + parseInt(seatPrice);
 
+        // GRAND total price calculator
         const grandtotalPrice = document.getElementById('grand-total').innerText;
         const convertedGrandTotalPrice = parseInt(totalPrice)
-        console.log( convertedGrandTotalPrice );
         document.getElementById('grand-total').innerText = convertedGrandTotalPrice + parseInt(seatPrice);
     });
 
 }
 
+// Coupon code applying part
 const applyBtn = document.getElementById('apply-btn');
 applyBtn.addEventListener('click', function () {
     const grandTotalPrice1 = document.getElementById('total-price').innerText;
@@ -68,7 +79,7 @@ applyBtn.addEventListener('click', function () {
     }
 })
 
-
+// After clicking next btn, success page showing part
 const nextBtn = document.getElementById('next-btn');
 nextBtn.addEventListener('click', function () {
     const grandTotalPriceForNextBtn = document.getElementById('grand-total').innerText;
@@ -81,7 +92,7 @@ nextBtn.addEventListener('click', function () {
     }
 })
 
-
+// continue button of Success page click event code
 const continueBtn = document.getElementById('continue-btn');
 continueBtn.addEventListener('click', function () {
     document.getElementById('success-pop-up').style.display='none';
